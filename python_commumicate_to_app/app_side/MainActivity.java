41baad355682113f7e358a3ab41a14a2d0a5b733
Howie <yong_zhang@163.com>
2019-01-21 09:58:28 +0800

    add new dir apppp_side and python_side

---
+++ python_commumicate_to_app/app_side/MainActivity.java
@@ -0,0 +1,83 @@
+package yong.webtest;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView)findViewById(R.id.text);
+
+        new Thread(networkTask).start();
+
+    }
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Bundle data = msg.getData();
+            String val = data.getString("value");
+            text.setText(val);
+        }
+    };
+
+    Runnable networkTask = new Runnable() {
+        @Override
+        public void run() {
+            while(true) {
+                try {
+                    Socket socket = new Socket("47.106.190.151", 55555);
+
+                    //获取输出流，向服务器端发送信息
+                    OutputStream os = socket.getOutputStream();//字节输出流
+                    PrintWriter pw = new PrintWriter(os);//将输出流包装为打印流
+                    pw.write("我是Java服务器");
+                    pw.flush();
+                    socket.shutdownOutput();//关闭输出流
+
+                    InputStream is = socket.getInputStream();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(is));
+                    String info = null;
+                    while ((info = in.readLine()) != null) {
+                        //System.out.println("我是客户端，Python服务器说："+info);
+                        Log.w("MAIN", "我是客户端，Python服务器说：" + info);
+                        Message msg = new Message();
+                        Bundle data = new Bundle();
+                        data.putString("value", "我是客户端，Python服务器说：" + info);
+                        msg.setData(data);
+                        handler.sendMessage(msg);
+                    }
+                    is.close();
+                    in.close();
+                    socket.close();
+                } catch (UnknownHostException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+}
